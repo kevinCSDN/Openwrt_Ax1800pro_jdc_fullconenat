1fcc09ff18bf3d59b04fadae9c5fe64302b9156c
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2024-06-02 12:39:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,7 @@
 # Uncomment a feed source
 #sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '1i src-git kiddin https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+#sed -i '1i src-git kiddin https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
@@ -22,11 +22,11 @@ git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 ./scripts/feeds install -a 
 #rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
 
-#git clone https://github.com/kiddin9/openwrt-packages
-#shopt -s extglob
+git clone https://github.com/kiddin9/openwrt-packages
+shopt -s extglob
 #rm -rf openwrt-packages/!luci-app-netdata
-#cp -r openwrt-packages/{luci-app-netdata,firewall4,luci-app-fullconenat} package/
-#rm -rf openwrt-packages
+cp -r openwrt-packages/luci-app-turboacc package/
+rm -rf openwrt-packages
 
 #rm -rf package/network/config/firewall
 #git clone https://github.com/fullcone-nat-nftables/openwrt-firewall4-with-fullcone.git package/network/config/firewall
